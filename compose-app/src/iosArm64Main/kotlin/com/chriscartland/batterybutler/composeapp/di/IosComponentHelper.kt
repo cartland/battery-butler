c1fefd0f6c2ec8cd86df8471b61dbe23948e5ada
cartland <c.cartland@gmail.com>
2026-01-16 10:03:51 -0800

    fix(ios): update imports in IosComponentHelper.kt
    Fixed import of com.chriscartland.batterybutler.iosswiftdi and com.chriscartland.batterybutler.composeapp.di after package refactoring. Applied Spotless formatting.

--- compose-app/src/iosArm64Main/kotlin/com/chriscartland/batterybutler/composeapp/di/IosComponentHelper.kt
+++ compose-app/src/iosArm64Main/kotlin/com/chriscartland/batterybutler/composeapp/di/IosComponentHelper.kt
@@ -1,8 +1,8 @@
 package com.chriscartland.batterybutler.composeapp.di
 
+import com.chriscartland.batterybutler.composeapp.di.create
 import com.chriscartland.batterybutler.composeapp.feature.ai.NoOpAiEngine
 import com.chriscartland.batterybutler.data.di.DatabaseFactory
-import com.chriscartland.batterybutler.composeapp.di.create
 import com.chriscartland.batterybutler.domain.repository.RemoteDataSource
 import com.chriscartland.batterybutler.domain.repository.RemoteUpdate
 import kotlinx.coroutines.flow.Flow
