f9ad67f34933d1e06368e812b62e3ef45fd52f41
cartland <c.cartland@gmail.com>
2026-01-16 10:57:52 -0800

    fix: update run scripts for android, server, and unit tests

--- scripts/run-android-app.sh
+++ scripts/run-android-app.sh
@@ -9,4 +9,4 @@ set -ex
 
 # Installs and runs the Android application on a connected device or emulator.
 ./gradlew :compose-app:installDebug
-adb shell am start -n com.chriscartland.batterybutler/.android.MainActivity
+adb shell am start -n com.chriscartland.batterybutler/.composeapp.MainActivity
--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./gradlew :server:run
+./gradlew :server:app:run
