5e3a29ab8dc20a3839951f45d7f384cfc1fd92ef
cartland <c.cartland@gmail.com>
2026-01-19 00:26:38 -0800

    Run ./scripts/spotless-apply.sh

--- presentation-core/src/commonMain/kotlin/com/chriscartland/batterybutler/presentationcore/components/HistoryListItem.kt
+++ presentation-core/src/commonMain/kotlin/com/chriscartland/batterybutler/presentationcore/components/HistoryListItem.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.time.Instant
 import com.chriscartland.batterybutler.domain.model.BatteryEvent
 import com.chriscartland.batterybutler.presentationcore.theme.BatteryButlerTheme
 import kotlinx.datetime.TimeZone
