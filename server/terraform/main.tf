9f57f7b037d0465072ba67b0505543637d7a6dbc
cartland <c.cartland@gmail.com>
2026-01-21 01:01:59 -0800

    Chore: Configure Terraform S3 Backend

--- server/terraform/main.tf
+++ server/terraform/main.tf
@@ -6,8 +6,16 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "battery-butler-tf-state-cartland-dev"
+    key            = "server/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "battery-butler-tf-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
