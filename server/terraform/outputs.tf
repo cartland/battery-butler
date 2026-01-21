821c16023f6e48994471d43fc5bdaacfae65a237
cartland <c.cartland@gmail.com>
2026-01-21 02:16:24 -0800

    Fix: Configure Jib to push to AWS ECR instead of Docker Hub

--- server/terraform/outputs.tf
+++ server/terraform/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.main.dns_name
 }
+
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.server.repository_url
+}
