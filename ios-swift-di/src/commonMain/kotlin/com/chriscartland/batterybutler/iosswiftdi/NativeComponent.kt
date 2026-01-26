93db318e927cd22e845f8ce0bcd1451ce70a76c2
cartland <c.cartland@gmail.com>
2026-01-26 00:04:59 -0800

    style: Fix formatting in NativeComponent

--- ios-swift-di/src/commonMain/kotlin/com/chriscartland/batterybutler/iosswiftdi/NativeComponent.kt
+++ ios-swift-di/src/commonMain/kotlin/com/chriscartland/batterybutler/iosswiftdi/NativeComponent.kt
@@ -71,9 +71,13 @@ abstract class NativeComponent(
     fun provideAppScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
     @Provides
-    fun provideAppVersion(): com.chriscartland.batterybutler.domain.model.AppVersion = com.chriscartland.batterybutler.domain.model.AppVersion.Ios("iOS Native")
+    fun provideAppVersion(): com.chriscartland.batterybutler.domain.model.AppVersion =
+        com.chriscartland.batterybutler.domain.model.AppVersion
+            .Ios("iOS Native")
 
     @Provides
     @SharedSingleton
-    fun provideAppInfoRepository(impl: com.chriscartland.batterybutler.data.repository.StaticAppInfoRepository): com.chriscartland.batterybutler.domain.repository.AppInfoRepository = impl
+    fun provideAppInfoRepository(
+        impl: com.chriscartland.batterybutler.data.repository.StaticAppInfoRepository,
+    ): com.chriscartland.batterybutler.domain.repository.AppInfoRepository = impl
 }
