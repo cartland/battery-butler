1fb7b9a71ca2d0b326f2f0899f189012d69ff6a8
cartland <c.cartland@gmail.com>
2026-01-26 22:42:09 -0800

    chore: consolidate Spotless configuration to root build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,18 +1,10 @@
 plugins {
     `kotlin-dsl`
-    id("com.diffplug.spotless") version "7.0.4"
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
-}
-
-spotless {
-    kotlin {
-        target("src/**/*.kt")
-        ktlint()
-    }
 }
 
 gradlePlugin {
